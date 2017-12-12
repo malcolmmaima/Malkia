1f7e03aec7d45a9372e0ecf7c061b2b68bdd4a4d
Maima <malcolmmaima@gmail.com>
2017-12-12 14:31:09 +0300

    Temporary: Splash to MainActivity

--- app/src/main/java/malkia/malkiaunesco/example/com/malkia/SplashActivity.java
+++ app/src/main/java/malkia/malkiaunesco/example/com/malkia/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 // This method will be executed once the timer is over
-                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(i);
                 // close this activity
                 finish();
