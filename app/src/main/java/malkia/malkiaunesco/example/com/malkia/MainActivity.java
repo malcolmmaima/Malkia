990f315e2fb4aa84acf36dd1233cfca31b9cdb45
dezma <dezmondmaina@gmail.com>
2017-12-12 13:13:27 +0300

    login implementtion

--- app/src/main/java/malkia/malkiaunesco/example/com/malkia/MainActivity.java
+++ app/src/main/java/malkia/malkiaunesco/example/com/malkia/MainActivity.java
@@ -2,6 +2,22 @@ package malkia.malkiaunesco.example.com.malkia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
 
