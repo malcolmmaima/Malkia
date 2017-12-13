2e1e42e2a81dd7a02b9078965e5e54153147ba75
UshieYvette <yvetteahono97@gmail.com>
2017-12-13 15:33:43 +0300

    Implemented About Module: CardView

---
+++ app/src/main/java/malkia/malkiaunesco/example/com/malkia/activitys/AboutActivity.java
@@ -0,0 +1,19 @@
+package malkia.malkiaunesco.example.com.malkia.activitys;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import malkia.malkiaunesco.example.com.malkia.R;
+
+/**
+ * Created by USER on 12/13/2017.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+}
+}
